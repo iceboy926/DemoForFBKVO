3e96136a9cb224963b352c51efb1f0ec6be9f1ab
sfmDev <sfmdeveloper@icloud.com>
2015-11-29 10:04:29 +0800

    firsr commit

---
+++ FBKVOTest/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  FBKVOTest
+//
+//  Created by 石峰铭 on 15/11/29.
+//  Copyright © 2015年 shifengming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ FBKVOTest/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  FBKVOTest
+//
+//  Created by 石峰铭 on 15/11/29.
+//  Copyright © 2015年 shifengming. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
